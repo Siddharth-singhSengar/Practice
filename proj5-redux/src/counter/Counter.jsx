3bbb0537c252181035cc86f9580901c69699e0ba
Siddharth-singhSengar <156781416+Siddharth-singhSengar@users.noreply.github.com>
2025-04-12 16:04:01 +0530

    update code

--- proj5-redux/src/counter/Counter.jsx
+++ proj5-redux/src/counter/Counter.jsx
@@ -7,7 +7,7 @@ const Counter = () =>{
     const dispatch = useDispatch();
     return (
         <div>
-          <h1>Counter: {count}</h1>
+          <h1>Counter  : {count}</h1>
           <button onClick={() => dispatch(increment())}>Increment</button>
           <button onClick={() => dispatch(decrement())}>Decrement</button>
           <button onClick={() => dispatch(reset())}>Reset</button>
